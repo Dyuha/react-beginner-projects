82c2be1df1c3832fdaefa0e439cbe116792c7f4c
Dyuha <iamdyuha@tut.by>
2023-01-26 14:08:22 +0300

    eazy peazy quiz

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import { useState } from 'react';
 
 const questions = [
   {
@@ -22,38 +23,66 @@ const questions = [
   },
 ];
 
-function Result() {
+const Result = ({ onClickReset, correctAnswerCount, numderOfQuestions }) => {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt='qwerty'/>
+      <h2>{`Вы отгадали ${correctAnswerCount} ответа из ${numderOfQuestions}`}</h2>
+      <button onClick={() => onClickReset()} >Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+const Game = ({question, onClickVariant, progressBar}) => {
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${progressBar}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {
+          question.variants.map((variant, index) => 
+            <li onClick={() => onClickVariant(index)} key={index}>{variant}</li>)
+        }
       </ul>
     </>
   );
 }
 
 function App() {
+
+  const numderOfQuestions = questions.length
+  const progressBarIncrement = 100 / numderOfQuestions;
+
+  const [step, setStep] = useState(0);
+  const [correctAnswerCount, setCorrectAnswer] = useState(0);
+  const [progressBar, setProgressBar] = useState(progressBarIncrement)
+  const question = questions[step];  
+  
+  const onClickVariant = (index) => {
+    if(step < numderOfQuestions){
+      setStep(step + 1);
+      setProgressBar(progressBar + progressBarIncrement);
+    }
+    if (index === question.correct){
+      setCorrectAnswer(correctAnswerCount + 1)
+    }
+  }
+
+  const onClickReset = () => {
+    setStep(0);
+    setCorrectAnswer(0);
+    setProgressBar(progressBarIncrement);
+  }
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
-    </div>
+      { step === questions.length
+        ? <Result onClickReset={onClickReset} correctAnswerCount={correctAnswerCount} numderOfQuestions={numderOfQuestions}/>
+        : <Game question={question} onClickVariant={onClickVariant} progressBar={progressBar}/>
+      }
+   </div>
   );
 }
 
